2acf4d51c6951c4e42310446378a189d947e26a1
dojt <dotheis.work@gmail.com>
2020-04-06 20:21:41 +0300

    WIP

--- src/QuEST.jl
+++ src/QuEST.jl
@@ -10,7 +10,6 @@ using .QuEST_h
 #
 #    - create
 #    - destroy
-#    - sync
 #
 #-------------------------------------------------------------------------------
 
@@ -25,11 +24,11 @@ function destroyQuESTEnv(env ::QuESTEnv) ::Nothing
     return nothing
 end
 
-function syncQuESTEnv(env ::QuESTEnv) ::Nothing
-    ccall(:syncQuESTEnv, Cvoid, (QuESTEnv,),
-          env)
-    return nothing
-end
+# function syncQuESTEnv(env ::QuESTEnv) ::Nothing
+#     ccall(:syncQuESTEnv, Cvoid, (QuESTEnv,),
+#           env)
+#     return nothing
+# end
 
 ## Qureg #----------------------------------------------------------------------
 #
@@ -171,8 +170,87 @@ function initDebugState(qureg ::Qureg) ::Nothing
     return nothing
 end
 
-initStateFromAmps(Qureg qureg, reals ::Vector{Qreal}, imags ::Vector{Qreal} ) ::Nothing
-void initStateFromAmps(Qureg qureg, qreal* reals, qreal* imags);
+function initStateFromAmps(qureg ::Qureg,
+                           reals ::Vector{Qreal},
+                           imags ::Vector{Qreal}  ) ::Nothing
+    ccall(:initStateFromAmps, Cvoid, (Qureg, Ptr{Qreal}, Ptr{Qreal}),
+          qureg, reals, imags)
+    return nothing
+end
+
+function setAmps(qureg    ::Qureg,
+                 startIdx ::Int64,
+                 reals    ::Vector{Qreal},
+                 imags    ::Vector{Qreal},
+                 numAmps  ::Int64)          :: Nothing
+    ccall(:setAmps, Cvoid, (Qureg, Clonglong, Ptr{Qreal}, Ptr{Qreal}, Clonglong),
+          qureg,  startIdx,  reals, imags, numAmps)
+    return nothing
+end
+
+function cloneQureg(targetQureg ::Qureg, copyQureg ::Qureg) ::Nothing
+    ccall(:cloneQureg, Cvoid, (Qureg, Qureg),
+          targetQureg, copyQureg)
+    return nothing
+end
+
+function phaseShift(qureg       ::Qureg,
+                    targetQubit ::Int,
+                    angle       ::Qreal) ::Nothing
+    ccall(:phaseShift, Cvoid, (Qureg, Cint, Qreal),
+           qureg, targetQubit, angle)
+    return nothing
+end
+
+function controlledPhaseShift(qureg    ::Qureg,
+                              idQubit1 ::Int,
+                              idQubit2 ::Int,
+                              angle    ::Qreal)   ::Nothing
+    ccall(:controlledPhaseShift, Cvoid, (Qureg, Cint, Cint, Qreal),
+          qureg, idQubit1, idQubit2, angle)
+    return nothing
+end
+
+function multiControlledPhaseShift(qureg         ::Qureg,
+                                   controlQubits ::Vector{Int32},
+                                   angle         ::Qreal)         ::Nothing
+    ccall(:multiControlledPhaseShift, Cvoid, (Qureg, Ptr{Cint}, Cint, Qreal)
+          qureg, controlQubits, length(controlQubits),  angle),
+    return nothing
+end
+
+function controlledPhaseFlip(qureg ::Qureg, idQubit1 ::Int, idQubit2 ::Int) ::Nothing
+    ccall(:controlledPhaseFlip, Cvoid, (Qureg, Cint, Cint),
+           qureg, idQubit1, idQubit2)
+    return nothing
+end
+
+function multiControlledPhaseFlip(qureg         ::Qureg,
+                                  controlQubits ::Vector{Int32}) ::Nothing
+    ccall(:multiControlledPhaseFlip, Cvoid, (Qureg, Ptr{Cint}, Cint),
+          qureg, controlQubits, length(controlQubits))
+    return nothing
+end
+
+function sGate(qureg ::Qureg, targetQubit ::Int) ::Nothing
+    ccall(:sGate, Cvoid, (Qureg, Cint),
+           qureg,  targetQubit)
+    return nothing
+end
+
+function tGate(qureg ::Qureg, targetQubit ::Int) ::Nothing
+    ccall(:tGate, Cvoid, (Qureg, Cint),
+          qureg, targetQubit)
+    return nothing
+end
+
+function reportQuESTEnv(env ::QuESTEnv) ::Nothing
+    ccall(:reportQuESTEnv, Cvoid, (QuESTEnv,),
+          env)
+    return nothing
+end
+
+
 
 
 
