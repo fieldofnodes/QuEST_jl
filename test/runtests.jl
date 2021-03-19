032d45df30c7e3130b50349325558befe838c953
Bahman <b.ghandchi@gmail.com>
2021-03-19 16:01:42 +0200

    ready to push

--- test/runtests.jl
+++ test/runtests.jl
@@ -1169,7 +1169,8 @@ function test_multiRotateZ()
         θ = rand(qreal)
         
         c_targs = Array{Cint, 1}()
-        for ind =1:numQubits
+        push!(c_targs, 0)
+        for ind =2:numQubits
             if codes[ind] == 1 
                 push!(c_targs, ind-1)
             end
@@ -1222,7 +1223,7 @@ function test_multiStateControlledUnitary()
         numQubits = rand(3:12)
         qureg = QuEST.createQureg(numQubits, env)
         
-        codes = rand(0:2, numQubits)
+        codes = rand(0:1, numQubits)
         target = rand(1:numQubits) 
         codes[target] = 3
 
@@ -2514,16 +2515,14 @@ function test_calcExpecPauliProd()
         H=zeros(2^numQubits, 2^numQubits)
         
         H=ones(1, 1)
-        c_targs = Vector{Cint}()
-        c_codes = Vector{QuEST.QuEST_Types.pauliOpType}()
+        c_targs = Vector{Cint}([x for x in 0:numQubits-1])
+        #c_codes = Vector{QuEST.QuEST_Types.pauliOpType}()
 
         H=zeros(2^numQubits, 2^numQubits)
-        for term=0:numSumTerms-1
-            h_tmp=ones(1, 1)
-            for qubit =0:numQubits-1
-                h_tmp = kron(pauli_dict[codes[term*numQubits+qubit+1]], h_tmp)
-            end
-            H += coeffs[term+1]*h_tmp
+        H=ones(1, 1)
+        h_tmp=ones(1, 1)
+        for qubit =0:numQubits-1
+            H = kron(pauli_dict[codes[qubit+1]], H)
         end
 
 
@@ -2535,7 +2534,7 @@ function test_calcExpecPauliProd()
 
         lhs = transpose(conj(state))*H*state
 
-        rhs = QuEST.calcExpecPauliProd(qureg, c_targs, c_codes, work)
+        rhs = QuEST.calcExpecPauliProd(qureg, c_targs, codes, work)
 
         @test rhs ≈ real(lhs)
         
@@ -3354,20 +3353,22 @@ function test_reportPauliHamil()
     QuEST.destroyQuESTEnv(env)
 end
 
-function test_report()
+function test_reportQuregParams()
 
     env= QuEST.createQuESTEnv()
-    QuEST.reportQuESTEnv(env)
     numQubits = rand(3:12)
     qureg = QuEST.createQureg(numQubits, env)
+
 
     original_stdout = stdout
     read_pipe, write_pipe = redirect_stdout()
     lines = Vector{String}()
+
     QuEST.reportQuregParams(qureg)
     Base.Libc.flush_cstdio()
     redirect_stdout(original_stdout)
     close(write_pipe)
+
     for l =1:5
         push!(lines, readline(read_pipe))
     end
@@ -3376,6 +3377,33 @@ function test_report()
     @test lines[2] == "Number of qubits is $numQubits."
     @test lines[3] == "Number of amps is $(2^numQubits)."
     @test lines[5] == ""
+
+    QuEST.destroyQureg(qureg, env)
+    QuEST.destroyQuESTEnv(env)
+end
+
+function test_reporQuESTEnv()
+    
+    env= QuEST.createQuESTEnv()
+    numQubits = rand(3:12)
+    qureg = QuEST.createQureg(numQubits, env)
+
+    original_stdout = stdout
+    read_pipe, write_pipe = redirect_stdout()
+    lines = Vector{String}()
+
+    QuEST.reportQuESTEnv(env)
+    Base.Libc.flush_cstdio()
+    redirect_stdout(original_stdout)
+
+    close(write_pipe)
+    
+    for l =1:7
+        push!(lines, readline(read_pipe))
+    end
+
+    @test lines[1] == "EXECUTION ENVIRONMENT:"
+    @test lines[end] == ""
 
     QuEST.destroyQureg(qureg, env)
     QuEST.destroyQuESTEnv(env)
@@ -3463,119 +3491,346 @@ function test_sync()
     QuEST.destroyQuESTEnv(env)
 
 end
-@testset "QuEST.jl" begin
+#@testset "QuEST.jl" begin
+
+@testset "test_env" begin
     test_env()
-    test_qureg()
-    test_qureg_density()
-    check_createCloneQureg()
-    check_ComplexMatrixN()
-    test_DiagonalOp()
-    test_PauliHaiml()
-    test_compactUnitary()
-    test_controlledCompactUnitary()
-    test_controlledMultiQubitUnitary()
-    test_controlledNot()
-    test_controlledPauliY()
-    test_controlledPhaseFlip()
-    test_controlledPhaseShift()
-    test_controlledRotateAroundAxis()
-    test_controlledRotateX()
-    test_controlledRotateY()
-    test_controlledRotateZ()
-    test_controlledTwoQubitUnitary()
-    test_controlledUnitary()
-    test_hadamard()
-    test_multiControlledMultiQubitUnitary()
-    test_multiControlledPhaseFlip()
-    test_multiControlledPhaseShift()
-    test_multiControlledTwoQubitUnitary()
-    test_multiControlledUnitary()
-    test_multiQubitUnitary()
-    test_multiRotatePauli()
-    test_multiRotateZ()
-    test_multiStateControlledUnitary()
-    test_pauliX()
-    test_pauliY()
-    test_pauliZ()
-    test_phaseShift()
-    test_rotateAroundAxis()
-    test_rotateX()
-    test_rotateY()
-    test_rotateZ()
-    test_sGate()
-    test_sqrtSwapGate()
-    test_swapGate()
-    test_tGate()
-    test_twoQubitUnitary()
-    test_unitary()
-
-    ##############
-    ##############
-
-    test_collapseToOutcome()
-    test_measure()
-    test_measureWithStats()
-    test_DiagonalOp()
-    test_applyMatrix2()
-    test_applyMatrix4()
-    test_applyMatrixN()
-    test_applyMultiControlledMatrixN()
-    test_applyPauliHamil()
-    test_applyPauliSum()
-    test_applyTrotterCircuit()
-
-    #####################
-    #####################
-
-    test_calcDensityInnerProduct()
-    test_calcExpecDiagonalOp()
-    test_calcExpecPauliHamil()
-    test_calcExpecPauliProd()
-    test_calcExpecPauliSum()
-    test_calcFidelity()
-    test_calcHilbertSchmidtDistance()
-    test_calcInnerProduct()
-    test_calcProbOfOutcome()
-    test_calcPurity()
-    test_calcTotalProb()
-    test_amp_funcs()
-    test_getDensityAmp()
-
-    ####################
-    ####################
-
-    test_cloneQureg()
-    test_initBlankState()
-    test_initClassicalState()
-    test_initPlusState()
-    test_initPureState()
-    test_initStateFromAmps()
-    test_initZeroState()
-    test_setAmps()
-    test_setWeightedQureg()
-
-    #################
-    #################
-
-    test_QASM()
-
-    ################
-    ################
-
-    test_GPU()
-    test_initDebugState()
-    test_reportPauliHamil()
-    test_reportPauliHamil()
-    test_report()
-    test_reportState()
-    test_seed()
-    test_sync()
-
-    ################
-    ################
-
-
-
 end
+
+@testset "test_qureg" begin
+    test_qureg()
+end
+
+@testset "test_qureg_density" begin
+    test_qureg_density()
+end
+
+@testset "check_createCloneQureg" begin
+    check_createCloneQureg()
+end
+
+@testset "check_ComplexMatrixN" begin
+    check_ComplexMatrixN()
+end
+
+@testset "test_DiagonalOp" begin
+    test_DiagonalOp()
+end
+
+@testset begin
+    test_PauliHaiml()
+end
+
+@testset "test_PauliHaiml" begin
+    test_compactUnitary()
+end
+
+@testset "test_controlledCompactUnitary" begin
+    test_controlledCompactUnitary()
+end
+
+@testset "test_controlledMultiQubitUnitary" begin
+    test_controlledMultiQubitUnitary()
+end
+
+@testset "test_controlledNot" begin    
+    test_controlledNot()
+end
+
+@testset begin    
+    test_controlledPauliY()
+end
+
+@testset "test_controlledPauliY" begin    
+    test_controlledPhaseFlip()
+end
+
+@testset "test_controlledPhaseShift" begin    
+    test_controlledPhaseShift()
+end
+
+@testset "test_controlledRotateAroundAxis" begin    
+    test_controlledRotateAroundAxis()
+end
+
+@testset "test_controlledRotateX" begin    
+    test_controlledRotateX()
+end
+
+@testset "test_controlledRotateY" begin    
+    test_controlledRotateY()
+end
+
+@testset "test_controlledRotateZ" begin    
+    test_controlledRotateZ()
+end
+
+@testset "test_controlledTwoQubitUnitary" begin    
+    test_controlledTwoQubitUnitary()
+end
+
+@testset "test_controlledUnitary" begin    
+    test_controlledUnitary()
+end
+
+@testset "test_hadamard" begin    
+    test_hadamard()
+end
+
+@testset "test_multiControlledMultiQubitUnitary" begin    
+    test_multiControlledMultiQubitUnitary()
+end
+
+@testset "test_multiControlledPhaseFlip" begin
+    test_multiControlledPhaseFlip()
+end
+
+@testset "test_multiControlledPhaseShift" begin
+    test_multiControlledPhaseShift()
+end
+
+@testset "test_multiControlledTwoQubitUnitary" begin
+    test_multiControlledTwoQubitUnitary()
+end
+
+@testset "test_multiControlledUnitary" begin
+    test_multiControlledUnitary()
+end
+
+@testset "test_multiQubitUnitary" begin
+    test_multiQubitUnitary()
+end
+
+@testset "test_multiRotatePauli" begin
+    test_multiRotatePauli()
+end
+
+@testset "test_multiRotateZ" begin
+    test_multiRotateZ()
+end
+
+@testset "test_multiStateControlledUnitary" begin
+    test_multiStateControlledUnitary()
+end
+
+@testset "test_pauliX" begin
+    test_pauliX()
+end
+
+@testset "test_pauliY" begin
+    test_pauliY()
+end
+
+@testset "test_pauliZ" begin
+    test_pauliZ()
+end
+
+@testset "test_phaseShift" begin
+    test_phaseShift()
+end
+
+@testset begin
+    test_rotateAroundAxis()
+end
+
+@testset "test_rotateAroundAxis" begin
+    test_rotateX()
+end
+
+@testset "test_rotateY" begin
+    test_rotateY()
+end
+
+@testset "test_rotateZ" begin
+    test_rotateZ()
+end
+
+@testset "test_sGate" begin
+    test_sGate()
+end
+
+@testset "test_sqrtSwapGate" begin
+    test_sqrtSwapGate()
+end
+
+@testset "test_swapGate" begin
+    test_swapGate()
+end
+
+@testset "test_tGate" begin
+    test_tGate()
+end
+
+@testset "test_twoQubitUnitary" begin
+    test_twoQubitUnitary()
+end
+
+@testset "test_unitary" begin
+    test_unitary()
+end
+
+    ##############
+    ##############
+
+@testset "test_collapseToOutcome" begin
+    test_collapseToOutcome()
+end
+
+@testset "test_measure" begin
+    test_measure()
+end
+
+@testset "test_measureWithStats" begin
+    test_measureWithStats()
+end
+
+@testset "test_DiagonalOp" begin
+    test_DiagonalOp()
+end
+
+@testset "test_applyMatrix2" begin
+    test_applyMatrix2()
+end
+
+@testset "test_applyMatrix4" begin
+    test_applyMatrix4()
+end
+
+@testset "test_applyMatrixN" begin
+    test_applyMatrixN()
+end
+
+@testset "test_applyMultiControlledMatrixN" begin
+    test_applyMultiControlledMatrixN()
+end
+
+@testset "test_applyPauliHamil" begin
+    test_applyPauliHamil()
+end
+
+@testset "test_applyPauliSum" begin
+    test_applyPauliSum()
+end
+
+@testset "test_applyTrotterCircuit" begin
+    test_applyTrotterCircuit()
+end
+
+    #####################
+    #####################
+@testset "test_calcDensityInnerProduct" begin
+    test_calcDensityInnerProduct()
+end
+@testset "test_calcExpecDiagonalOp" begin
+    test_calcExpecDiagonalOp()
+end
+@testset "test_calcExpecPauliHamil" begin
+    test_calcExpecPauliHamil()
+end
+@testset "test_calcExpecPauliProd" begin
+    test_calcExpecPauliProd()
+end
+@testset "test_calcExpecPauliSum" begin
+    test_calcExpecPauliSum()
+end
+@testset "test_calcFidelity" begin
+    test_calcFidelity()
+end
+@testset "test_calcHilbertSchmidtDistance" begin
+    test_calcHilbertSchmidtDistance()
+end
+@testset "test_calcInnerProduct" begin
+    test_calcInnerProduct()
+end
+@testset "test_calcProbOfOutcome" begin
+    test_calcProbOfOutcome()
+end
+@testset "test_calcPurity" begin
+    test_calcPurity()
+end
+@testset "test_calcTotalProb" begin
+    test_calcTotalProb()
+end
+@testset "test_amp_funcs" begin
+    test_amp_funcs()
+end
+@testset "test_getDensityAmp" begin
+    test_getDensityAmp()
+end
+
+    ####################
+    ####################
+@testset "test_cloneQureg" begin
+    test_cloneQureg()
+end
+@testset "test_initBlankState" begin
+    test_initBlankState()
+end
+@testset "test_initClassicalState" begin
+    test_initClassicalState()
+end
+@testset "test_initPlusState" begin
+    test_initPlusState()
+end
+@testset "test_initPureState" begin
+    test_initPureState()
+end
+@testset "test_initStateFromAmps" begin
+    test_initStateFromAmps()
+end
+@testset "test_initZeroState" begin
+    test_initZeroState()
+end
+@testset "test_setAmps" begin
+    test_setAmps()
+end
+@testset "test_setWeightedQureg" begin
+    test_setWeightedQureg()
+end
+
+    #################
+    #################
+
+@testset "test_QASM" begin
+    test_QASM()
+end
+
+    ###############
+    ###############
+
+@testset "test_GPU" begin
+    test_GPU()
+end
+@testset "test_initDebugState" begin
+    test_initDebugState()
+end
+@testset "test_reportPauliHamil" begin
+    test_reportPauliHamil()
+end
+
+@testset "test_reportQuregParams" begin
+    test_reportQuregParams()
+end
+
+@testset "test_reporQuESTEnv" begin
+    test_reporQuESTEnv()
+end
+
+@testset "test_reportState" begin
+    test_reportState()
+end
+@testset "test_seed" begin
+    test_seed()
+end
+@testset "test_sync" begin
+    test_sync()
+end
+
+    ################
+    ################
+
+
+
+#end
 
 # EOF
